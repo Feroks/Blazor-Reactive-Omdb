1c643ca7307d1f504db68aa610bad1a724e7bede
Mihail <mp@best.lv>
2019-08-08 13:36:16 +0300

    Created BasePage

---
+++ src/JE.App/Pages/BasePage.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Reactive.Linq;
+
+namespace JE.App.Pages
+{
+    public class BasePage<T> : ComponentBase, IDisposable
+        where T : BaseViewModel
+    {
+        private IDisposable _vmUpdateSubscription;
+
+        [Inject]
+        public T ViewModel { get; set; }
+
+        protected override void OnInit()
+        {
+            _vmUpdateSubscription = Observable
+                .FromEventPattern(
+                    h => ViewModel.StateHasChanged += h,
+                    h => ViewModel.StateHasChanged -= h)
+                .Subscribe(x => Invoke(StateHasChanged));
+        }
+
+        public void Dispose()
+        {
+            _vmUpdateSubscription.Dispose();
+            ViewModel.Dispose();
+        }
+    }
+}
