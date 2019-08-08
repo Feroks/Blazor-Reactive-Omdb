3497119cbf55bd11b02dad1b223001210fd79cd4
Mihail <mp@best.lv>
2019-08-08 11:22:24 +0300

    Added initial page for MovieList

---
+++ src/JE.App/Pages/BaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using JetBrains.Annotations;
+using ReactiveUI;
+using System;
+using System.Reactive.Disposables;
+
+namespace JE.App.Pages
+{
+    public class BaseViewModel : ReactiveObject, IDisposable
+    {
+        [NotNull]
+        protected CompositeDisposable CleanUp { get; } = new CompositeDisposable();
+
+        public void Dispose()
+        {
+            CleanUp.Dispose();
+        }
+    }
+}
---
+++ src/JE.App/Pages/Movie/List/MovieListViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using JE.Infrastructure.Services;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+
+namespace JE.App.Pages.Movie.List
+{
+    public class MovieListViewModel : BaseViewModel
+    {
+        [Reactive]
+        public string SearchText { get; set; }
+
+        public MovieListViewModel(IOmdbMovieService omdbMovieService)
+        {
+            this.WhenAnyValue(x => x.SearchText)
+                .Skip(1)
+                .SelectMany(omdbMovieService.SearchAsync)
+                .Subscribe()
+                .DisposeWith(CleanUp);
+        }
+    }
+}
