9aee0949072691cb0f2ddd7e232ed93fcbe31310
Mihail <mp@best.lv>
2019-08-08 11:26:34 +0300

    Use DynamicData

--- src/JE.App/Pages/Movie/List/MovieListViewModel.cs
+++ src/JE.App/Pages/Movie/List/MovieListViewModel.cs
@@ -4,21 +4,39 @@ using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
+using DynamicData;
+using DynamicData.Binding;
+using JE.Core.Dto;
 
 namespace JE.App.Pages.Movie.List
 {
     public class MovieListViewModel : BaseViewModel
     {
-        [Reactive]
-        public string SearchText { get; set; }
-
         public MovieListViewModel(IOmdbMovieService omdbMovieService)
         {
+            var source = new SourceCache<OmdbMovieSearchDto, string>(x => x.ImdbId)
+                .DisposeWith(CleanUp);
+
+            source.Connect()
+                .Sort(SortExpressionComparer<OmdbMovieSearchDto>.Ascending(p => p.Title), SortOptimisations.ComparesImmutableValuesOnly)
+                .Bind(Movies)
+                .Subscribe()
+                .DisposeWith(CleanUp);
+
             this.WhenAnyValue(x => x.SearchText)
                 .Skip(1)
                 .SelectMany(omdbMovieService.SearchAsync)
-                .Subscribe()
+                .Subscribe(x => source.Edit(list =>
+                {
+                    list.Clear();
+                    list.AddOrUpdate(x);
+                }))
                 .DisposeWith(CleanUp);
         }
+
+        [Reactive]
+        public string SearchText { get; set; }
+
+        public IObservableCollection<OmdbMovieSearchDto> Movies { get; } = new ObservableCollectionExtended<OmdbMovieSearchDto>();
     }
 }
